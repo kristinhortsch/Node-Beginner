e6c6581be9a4565b9a5cd372e146a4608209d2c2
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-02 21:03:25 -0800

    server, router, and request handlers successfully wired together- can see correct console logs in terminal when submitting /start request

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require("http");
+var url = require("url");
 
 /*one way to set up the server*/
 
@@ -8,9 +9,13 @@ var http = require("http");
   response.end();
 }).listen(8888);*/
 
-function start() {
+function start(route, handle) {
   function onRequest(request, response) {
-    console.log("Request received");
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+    route(handle, pathname);
+
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
